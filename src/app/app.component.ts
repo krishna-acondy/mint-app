b8a2623486b4c5ca59315a44c8e6978f49f244cf
Krishna Acondy <kacondy@Krishnas-MacBook-Pro.local>
2019-07-19 23:43:48 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mnt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mint-app';
 }
