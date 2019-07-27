271c5dc67d78e3e43d957d68970055afc33b4344
Krishna Acondy <kacondy@Krishnas-MacBook-Pro.local>
2019-07-27 13:50:33 +0100

    Made API URL configurable at runtime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from './state';
 import { configure } from './state/config';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'mnt-root',
@@ -9,7 +10,11 @@ import { configure } from './state/config';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private store: Store<AppState>) {
-    this.store.dispatch(configure({apiUrl: 'https://mnt-api.herokuapp.com/api'}));
+  constructor(
+    private httpClient: HttpClient,
+    private store: Store<AppState>) {
+      this.httpClient.get('assets/config.json').subscribe((config: any) => {
+      this.store.dispatch(configure({apiUrl: config.apiUrl}));
+      });
   }
 }
